008f3ad28c295724974b38cb1b4190e85d47c9db
Luis <ocasio.dev@gmail.com>
2020-07-20 22:45:44 -0400

    Implement the LS-8 Emulator

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -9,3 +9,20 @@ cpu = CPU()
 
 cpu.load()
 cpu.run()
+
+
+# LS-8 has 8-bit addressing, so can address 256 bytes of RAM total.
+
+# Execution Sequence
+
+# 1. The instruction pointed to by the `PC` is fetched from RAM, decoded, and executed.
+# 2. If the instruction does _not_ set the `PC` itself, the `PC` is advanced to point to the subsequent instruction.
+# 3. If the CPU is not halted by a `HLT` instruction, go to step 1.
+
+# INSTRUCTIONS TO IMPLEMENT
+
+# A. `LDI` - Set the value of a register to an integer.
+
+# B. `PRN`- Print numeric value stored in the given register.
+
+# C. 'HLT' INSTRUCTION.('HALTED')
